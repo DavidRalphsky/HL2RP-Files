1219d431c1f665d3784389bb418a4fb1cdf1c835
DavidRalphsky <garrysmodrox@yahoo.com>
2016-06-01 22:54:40 -0300

    Added comments

--- garrysmod/gamemodes/cwhl2rp/plugins/fastdl/plugin/sv_plugin.lua
+++ garrysmod/gamemodes/cwhl2rp/plugins/fastdl/plugin/sv_plugin.lua
@@ -1,29 +1,26 @@
 local PLUGIN = PLUGIN;
 local Clockwork = Clockwork;
 
-resource.AddWorkshop("297992925");
-resource.AddWorkshop("156174167");
-resource.AddWorkshop("105042805");
-resource.AddWorkshop("254077907");
-resource.AddWorkshop("147283087");
-resource.AddWorkshop("108727894");
-resource.AddWorkshop("150041615");
-resource.AddWorkshop("104491619");
-resource.AddWorkshop("104548572");
-resource.AddWorkshop("171962560");
-resource.AddWorkshop("104491619");
-resource.AddWorkshop("172378803");
-resource.AddWorkshop("175272156");
-resource.AddWorkshop("459563521");
-resource.AddWorkshop("339199543");
-resource.AddWorkshop("337998872");
-resource.AddWorkshop("338000038");
-resource.AddWorkshop("338001106");
-resource.AddWorkshop("338003457");
-resource.AddWorkshop("338001912");
-resource.AddWorkshop("338004845");
-resource.AddWorkshop("338005805");
-resource.AddWorkshop("338006371");
-resource.AddWorkshop("338007551");
-resource.AddWorkshop("453486598");
-resource.AddWorkshop("297864695");
+-- Note: This is a plugin for easy disabling/removing in case of some sort of problem
+
+resource.AddWorkshop("105042805"); -- Combine Administrators v2
+resource.AddWorkshop("254077907"); -- Half-Life 2 Beta Weapons Pack V2
+resource.AddWorkshop("147283087"); -- Halo's Content (Gas Masks, etc)
+resource.AddWorkshop("108727894"); -- Clockwork: HL2 RP Content
+resource.AddWorkshop("150041615"); -- TacoNBanana City 74 Model Pack
+resource.AddWorkshop("104491619"); -- Metropolice Pack
+resource.AddWorkshop("104548572"); -- Playable Piano
+resource.AddWorkshop("171962560"); -- rp_industrial17_v1
+resource.AddWorkshop("172378803"); -- Essential HL2RP Models
+resource.AddWorkshop("175272156"); -- Beta Metrocop Sounds
+resource.AddWorkshop("459563521"); -- Female Metropolice Pack
+resource.AddWorkshop("339199543"); -- Taco 'n' Banana Megapack: Materials Fix
+resource.AddWorkshop("337998872"); -- Taco 'n' Banana Megapack: Part 1
+resource.AddWorkshop("338000038"); -- Taco 'n' Banana Megapack: Part 2
+resource.AddWorkshop("338001106"); -- Taco 'n' Banana Megapack: Part 3
+resource.AddWorkshop("338001912"); -- Taco 'n' Banana Megapack: Part 4
+resource.AddWorkshop("338003457"); -- Taco 'n' Banana Megapack: Part 5
+resource.AddWorkshop("338004845"); -- Taco 'n' Banana Megapack: Part 6
+resource.AddWorkshop("338005805"); -- Taco 'n' Banana Megapack: Part 7
+resource.AddWorkshop("338006371"); -- Taco 'n' Banana Megapack: Part 8
+resource.AddWorkshop("338007551"); -- Taco 'n' Banana Megapack: Part 9
